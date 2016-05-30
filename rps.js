741dd9d8858c3f1bf5f26083f6c90dcf26aca2ba
winston mclean <zorba@winstons-MacBook-Pro.local>
2016-05-30 14:48:14 -0400

    Two different rock paper scissors games

---
+++ rps.js
@@ -0,0 +1,79 @@
+//does anything go at the top of js scripts?
+//opening message introduction the game
+//gets the users choice
+/*var userChoice = prompt("Do you choose rock, paper, or scissors?");*/
+//store each players choice in a
+
+var userChoice = "paper"
+//get the computers choice
+function computerChoice() {
+  var computerPick = Math.random();
+  if (computerPick <= 0.33) {
+    return "rock";
+  } else if (computerPick >= 0.34 && computerPick <= 0.66) {
+    return "paper";
+  } else {
+    return "scissors"
+  }
+};
+
+function compare(user, computer) {
+  //check for tie
+  if (user === computer) {
+    return "tie";
+  } else if (user === "rock") {
+    if (computer === "paper") {
+      return "computer";
+    } else {
+      return "user";
+    }
+  } else if (user === "paper") {
+    if (computer === "rock") {
+      return "user";
+    } else {
+      return "computer";
+    }
+  } else if (user === "scissors") {
+    if (computer === "rock") {
+      return "computer";
+    } else {
+      return "user";
+    }
+  }
+};
+
+function game() {
+  //variable to keep track of wins
+  var userWins = 0;
+  var computerWins = 0;
+  var ties = 0;
+  //contiune playing the game until someone gets to five wins
+  while (true) {
+    //checking to see who won each round
+    var winner = compare(userChoice, computerChoice());
+    //checks to see if someone has five wins
+    if (userWins === 5 || computerWins === 5) {
+      break;
+      //if no one has 5 wins, continue playing the game
+    } else {
+      if (winner === "tie") {
+      ties += 1;
+      console.log("It is a tie!")
+    } else if(winner === "user") {
+      userWins += 1;
+      console.log("The User has won this round!")
+    } else {
+      computerWins += 1;
+      console.log("The computer has Won this round!")
+    }
+  }
+  }
+  //checks to see who won then prints out a congradulations to the winner
+  if (userWins > computerWins) {
+   console.log("The User has won Rock Paper Scissors!");
+ } else {
+  console.log("The Computer has won Rock Paper Scissors!");
+ }
+}
+
+console.log(game());
